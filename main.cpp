cdbdfe32dd92fcc6f199b601d6bc79a133713d0e
Sohail Maqsood <160104056+iamsohailmaqsood14@users.noreply.github.com>
2024-05-12 23:23:47 +0500

    Update main.cpp
    I have added review class in this file.

--- main.cpp
+++ main.cpp
@@ -3,3 +3,21 @@
 #include <fstream>
 
 using namespace std;
+
+// Define the Review class
+class Review {
+private:
+    string text;
+    int rating; // Rating on a scale of 1 to 5
+
+public:
+    // Default constructor
+    Review() : text(""), rating(0) {}
+
+    // Constructor with parameters
+    Review(const string& _text, int _rating) : text(_text), rating(_rating) {}
+
+    // Getter functions
+    string getText() const { return text; }
+    int getRating() const { return rating; }
+};
